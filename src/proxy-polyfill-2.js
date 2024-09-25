5ddda64fa468173b90a49f2906baf6330193a905
Harman Goei <5001432+hrgui@users.noreply.github.com>
2024-09-24 19:33:57 -0700

    feat: switch proxy polyfill again

--- src/proxy-polyfill-2.js
+++ src/proxy-polyfill-2.js
@@ -1,207 +1,176 @@
-/**
- * Partial polyfill for Proxy. For details, see https://gist.github.com/mailmindlin/640e9d707ae3bd666d70
- */
-function Proxy(target, handler, revocable) {
-  var self = this; //because life
-  //override Object.prototype properties
-  Object.defineProperty(this, "__lookupGetter__", {
-    value: target.__lookupSetter__.bind(target),
-  });
-  Object.defineProperty(this, "__lookupSetter__", {
-    value: target.__lookupSetter__.bind(target),
-  });
-  Object.defineProperty(this, "__defineGetter__", {
-    value: target.__defineGetter__.bind(target),
-  });
-  Object.defineProperty(this, "__defineSetter__", {
-    value: target.__defineSetter__.bind(target),
-  });
-  Object.defineProperty(this, "toString", { value: target.toString.bind(target) });
-  Object.defineProperty(this, "valueOf", { value: target.valueOf.bind(target) });
-  Object.defineProperty(this, "__proto__", {
-    get: function () {
-      if ("getPrototypeOf" in handler) return handler.getPrototypeOf(target);
-      else return target.__proto__;
-    },
-    set: function (val) {
-      if ("setPrototypeOf" in handler) handler.setPrototypeOf(target, val);
-      else target.__proto__ = val;
-    },
-  }); //let's see if this is a bad idea
-
-  //for handler.apply
-  Object.defineProperty(this, "apply", {
-    value: function () {
-      if ("apply" in handler)
-        return handler.apply.apply(handler, target, this == self ? handler : this, arguments);
-      else return target.apply.apply(this == self ? target : this, arguments);
-    },
-  });
-
-  //for handler.has (not a full implementation, but as good as I could do)
-  Object.defineProperty(this, "hasOwnProperty", {
-    value: function (property) {
-      if ("has" in handler) {
-        return handler.has(target, property);
-      } else {
-        return target.hasOwnProperty(property);
-      }
-    },
-  });
-
-  //default getters and setters, to either update the target object or to call the handler's function
-  var get, set;
-  if ("get" in handler)
-    get = function (property) {
-      return handler.get(target, property, this);
-    };
-  else
-    get = function (property) {
-      return target[property];
-    };
-
-  if ("set" in handler)
-    set = function (property, value) {
-      handler.set(target, property, value, this);
-    };
-  else
-    set = function (property, value) {
-      targetObserver.doIgnore(property, "update");
-      target[property] = value;
-    };
-
-  //Functions to be used with Object.observe
-  var selfObserver = function (changes) {
-    changes.forEach(
-      function (change) {
-        if ((selfObserver.ignore[change.name] || []).includes(change.type)) {
-          delete selfObserver.ignore[change.name];
-          return;
-        }
-        if (change.type == "add") {
-          if ("defineProperty" in handler) {
-            handler.definePropery(
-              target,
-              change.name,
-              Object.getOwnPropertyDescriptor(target, key)
-            );
-          } else {
-            targetObserver.doIgnore(change.name, "add", "reconfigure", "update");
-            Object.defineProperty(
-              target,
-              change.name,
-              Object.getOwnPropertyDescriptor(this, change.name)
-            );
-            bind(change.name); //update getters/setters
+(() => {
+  var t = {
+      161: function (t) {
+        t.exports = function () {
+          let t;
+          function e(t) {
+            return !!t && ("object" == typeof t || "function" == typeof t);
           }
-        } else if (change.type == "delete") {
-          if ("deleteProperty" in handler) {
-            targetObserver.doIgnore(change.name, "delete");
-            if (!handler.deleteProperty(target, change.name)) {
-              targetObserver.dontIgnore(change.name, "delete");
-              bind(change.name);
-            }
-          } else {
-            targetObserver.doIgnore(change.name, "delete");
-            if (!delete target[change.name]) {
-              targetObserver.dontIgnore(change.name, "delete"); //it wasn't deleted successfully
-              bind(change.name);
-            }
+          function r(t) {
+            if (null !== t && !e(t))
+              throw TypeError("Object prototype may only be an Object or null: " + t);
           }
-        }
-      }.bind(this)
-    );
-  }.bind(this);
-  selfObserver.ignore = {};
-  selfObserver.doIgnore = function () {
-    selfObserver.ignore[arguments[0]] = Array.prototype.slice.call(arguments, 1);
-  };
-  selfObserver.dontIgnore = function () {
-    var name = arguments[0];
-    if (!(name in selfObserver.ignore)) return;
-    var events = Array.prototype.slice(arguments, 1);
-    for (var i in events)
-      if (selfObserver.ignore[name].includes(events[i]))
-        selfObserver.ignore[name].splice(selfObserver.ignore[name].indexOf(events[i]), 1);
-    if (selfObserver.ignore[name].length == 0) delete selfObserver.ignore[name];
-  };
-
-  var targetObserver = function (changes) {
-    changes.forEach(function (change) {
-      if ((targetObserver.ignore[change.name] || []).includes(change.type)) {
-        delete targetObserver.ignore[change.name];
-        return;
-      }
-      if (change.type == "add" || change.type == "reconfigure" || change.type == "update") {
-        bind(change.name);
-      } else if (change.type == "delete") {
-        selfObserver.doIgnore(change.name, "delete");
-        if (!delete this[change.name]) selfObserver.dontIgnore(change.name, "delete"); //TODO check why
-      }
-    });
-    targetObserver.ignore = {};
-  }.bind(this);
-  targetObserver.ignore = {};
-  targetObserver.doIgnore = function () {
-    targetObserver.ignore[arguments[0]] = Array.prototype.slice.call(arguments, 1);
-  };
-  targetObserver.dontIgnore = function () {
-    var name = arguments[0];
-    if (!(name in targetObserver.ignore)) return;
-    var events = Array.prototype.slice(arguments, 1);
-    for (var i in events)
-      if (targetObserver.ignore[name].includes(events[i]))
-        targetObserver.ignore[name].splice(targetObserver.ignore[name].indexOf(events[i]), 1);
-    if (targetObserver.ignore[name].length == 0) delete targetObserver.ignore[name];
-  };
-
-  //function to bind the getter and setter to the given object
-  var bind = function (key) {
-    var descriptor = Object.getOwnPropertyDescriptor(target, key);
-
-    //Make sure that these updates won't be sent to the handler
-    selfObserver.doIgnore(key, "add", "reconfigure");
-
-    //Set the property
-    Object.defineProperty(this, key, {
-      configurable: descriptor.configurable,
-      enumerable: descriptor.enumerable,
-      get: get.bind(this, key),
-      set: set.bind(this, key),
-    });
-  }.bind(this);
-
-  //bind to each existing property
-  for (var key in target) bind(key);
-
-  //Use Object.observe if supported, for more functionality (mirror properties as they are added, delete operator).
-  //As of Sept. 2015, only Chrome 36+ and Opera 23+ have Object.observe supported, so if you are using IE or Safari,
-  //this won't help you (if you're using Firefox, Proxy is built in, so you don't have a problem).
-  if ("observe" in Object) {
-    Object.observe(this, selfObserver);
-    Object.observe(target, targetObserver);
-  } else {
-    selfObserver = undefined;
-    targetObserver = undefined;
+          let o = Object,
+            n = !!o.create || !({ __proto__: null } instanceof o),
+            i =
+              o.create ||
+              (n
+                ? function (t) {
+                    return r(t), { __proto__: t };
+                  }
+                : function (t) {
+                    if ((r(t), null === t))
+                      throw SyntaxError(
+                        "Native Object.create is required to create objects with null prototype"
+                      );
+                    var e = function () {};
+                    return (e.prototype = t), new e();
+                  }),
+            u = function () {
+              return null;
+            },
+            c =
+              o.getPrototypeOf ||
+              ([].__proto__ === Array.prototype
+                ? function (t) {
+                    let r = t.__proto__;
+                    return e(r) ? r : null;
+                  }
+                : u);
+          function p(t, p, l) {
+            let f;
+            if (!e(t) || !e(p))
+              throw TypeError("Cannot create proxy with a non-object as target or handler");
+            let a = function (t) {},
+              s = p;
+            for (let t in ((p = { get: null, set: null, apply: null, construct: null }), s)) {
+              if (!(t in p)) {
+                console.warn(`Proxy polyfill does not support trap '${t}'`),
+                  (p[t] = function () {});
+                continue;
+              }
+              p[t] = s[t];
+            }
+            "function" == typeof s && (p.apply = s.apply.bind(s));
+            let y = c(t),
+              h = !1,
+              b = !1;
+            "function" == typeof t
+              ? ((f = function () {
+                  let e = this && this.constructor === f,
+                    r = Array.prototype.slice.call(arguments);
+                  return (a(e ? "construct" : "apply"), e && p.construct)
+                    ? p.construct.call(this, t, r)
+                    : !e && p.apply
+                    ? p.apply(t, this, r)
+                    : e
+                    ? (r.unshift(t), new (t.bind.apply(t, r))())
+                    : t.apply(this, r);
+                }),
+                (h = !0))
+              : t instanceof Array
+              ? ((f = []), (b = !0))
+              : (f = n || null !== y ? i(y) : {});
+            let _ = p.get
+                ? function (t) {
+                    return a("get"), p.get(this, t, f);
+                  }
+                : function (t) {
+                    return a("get"), this[t];
+                  },
+              d = p.set
+                ? function (t, e) {
+                    a("set"), p.set(this, t, e, f);
+                  }
+                : function (t, e) {
+                    a("set"), (this[t] = e);
+                  },
+              g = o.getOwnPropertyNames(t).map((e) => ({ name: e, source: t })),
+              w = y;
+            for (; w && w !== o.prototype; )
+              (g = g.concat(o.getOwnPropertyNames(w).map((t) => ({ name: t, source: w })))),
+                (w = o.getPrototypeOf(w));
+            let v = {};
+            g.forEach(function (e) {
+              let r = e.name,
+                n = e.source;
+              if (((h || b) && r in f) || v[r]) return;
+              let i = {
+                enumerable: !!o.getOwnPropertyDescriptor(n, r).enumerable,
+                get: _.bind(t, r),
+                set: d.bind(t, r),
+              };
+              o.defineProperty(f, r, i), (v[r] = !0);
+            });
+            let x = !0;
+            if (h || b) {
+              let t =
+                o.setPrototypeOf ||
+                ([].__proto__ === Array.prototype
+                  ? function (t, e) {
+                      return r(e), (t.__proto__ = e), t;
+                    }
+                  : u);
+              !(y && t(f, y)) && (x = !1);
+            }
+            if (p.get || !x)
+              for (let e in t) {
+                if (!v[e]) o.defineProperty(f, e, { get: _.bind(t, e) });
+              }
+            return (
+              o.seal(t),
+              o.seal(f),
+              l
+                ? {
+                    proxy: f,
+                    revoke: function () {
+                      (t = null),
+                        (a = function (t) {
+                          throw TypeError(`Cannot perform '${t}' on a proxy that has been revoked`);
+                        });
+                    },
+                  }
+                : f
+            );
+          }
+          return (
+            ((t = function (e, r) {
+              if (void 0 === (this && this instanceof t ? this.constructor : void 0))
+                throw TypeError("Constructor Proxy requires 'new'");
+              return p(e, r);
+            }).revocable = function (t, e) {
+              return p(t, e, !0);
+            }),
+            t
+          );
+        };
+      },
+    },
+    e = {};
+  function r(o) {
+    var n = e[o];
+    if (void 0 !== n) return n.exports;
+    var i = (e[o] = { exports: {} });
+    return t[o](i, i.exports, r), i.exports;
   }
-
-  //Add support for Proxy.revocable (kinda)
-  if (revocable == true)
-    this.revoke = function () {
-      if ("unobserve" in Object) {
-        Object.unobserve(this, selfObserver);
-        Object.unobserve(target, targetObserver);
-      }
-      selfObserver = undefined;
-      targetObserver = undefined;
-      bind = undefined;
-      get = undefined;
-      set = undefined;
-      for (var i in this) delete this[i];
-    }.bind(this);
-  return this;
-}
-Proxy.prototype = Function.prototype;
-Proxy.revocable = function (target, handler) {
-  return new Proxy(target, handler, true);
-};
+  (r.g = (function () {
+    if ("object" == typeof globalThis) return globalThis;
+    try {
+      return this || Function("return this")();
+    } catch (t) {
+      if ("object" == typeof window) return window;
+    }
+  })()),
+    (r.rv = function () {
+      return "1.0.7";
+    }),
+    (r.ruid = "bundler=rspack@1.0.7"),
+    (function (t) {
+      if (!t.Proxy) (t.Proxy = r(161)()), (t.Proxy.revocable = t.Proxy.revocable);
+    })(
+      ("undefined" != typeof process && "[object process]" === {}.toString.call(process)) ||
+        ("undefined" != typeof navigator && "ReactNative" === navigator.product)
+        ? r.g
+        : self
+    );
+})();
